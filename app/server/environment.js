3fc8f638351f30b5401d5df7e5e4f06723f8a4d5
David Sykora <dsykora@gmail.com>
2015-10-22 23:27:51 -0700

    Twitter config

--- app/server/environment.js
+++ app/server/environment.js
@@ -1,1 +1,11 @@
+ServiceConfiguration.configurations.upsert(
+	{ service: "twitter" },
+	{
+		$set: {
+			consumerKey: Meteor.settings && Meteor.settings.twitter && Meteor.settings.twitter.consumerKey,
+			secret:Meteor.settings && Meteor.settings.twitter &&  Meteor.settings.twitter.secret
+		}
+	}
+);
+
 TWILIO_PHONE_NUMBER = Meteor.settings && Meteor.settings.twilio && Meteor.settings.twilio.number;
